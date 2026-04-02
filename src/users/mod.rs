c0f6fb6b47a4f8ae9ecd1a301b0a701eeb297394
shroomiii <Primiti_ve@hotmail.com>
2026-04-03 00:18:29 +0100

    add publishing workflow

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,9 +1,9 @@
-/// user api stuff!
-/// 
-/// # endpoints
-/// 
-/// - profile (`/users/{id}/profile`)
-/// - friends (`/users/{id}/friends`)
+//! user api stuff!
+//! 
+//! # endpoints
+//! 
+//! - profile (`/users/{id}/profile`)
+//! - friends (`/users/{id}/friends`)
 
 pub mod profile;
 pub mod friends;
